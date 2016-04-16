70ba5eeffea39a3b295993c308aa3c59b0effcf1
kroton <kroton.prog@gmail.com>
2016-04-17 08:51:28 +0900

    first commit

---
+++ gg.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+red=31
+green=32
+yellow=33
+blue=34
+
+function cecho {
+	local color="${1}"
+	local msg="${2}"
+	printf "\033[${color}m${msg}\033[m\n"
+}
+
+function usage {
+	cat << EOF
+Usage:
+    gg <command> [<args>]
+
+SubCommands:
+    gg build
+    gg run
+    gg test [<target>]
+EOF
+}
+
+function build {
+	g++ -O2 -std=c++11 main.cpp -o main
+	return $?
+}
+
+function test_at {
+	cecho $green "test at ${1}"
+	./main < "${1}"	
+}
+
+function run_test {
+	local target="${1}"
+	if [ -z "${target}" ] || [ "${target}" = "all" ]; then
+		for input_file in `find . -name "*.in"`
+		do
+			test_at "${input_file}"
+		done
+	else
+		test_at "${target}.in"
+	fi
+}
+
+
+case "${1}" in
+	"build")
+		build && cecho $green "build succeeded"
+	;;
+
+	"run")
+		build && cecho $green "build succeeded and running" && ./main
+	;;
+
+	"test")
+		build && run_test "${2}"
+	;;
+
+	"help"|"--help"|"-h")
+		usage
+	;;
+
+	*)
+		usage
+	;;
+esac
